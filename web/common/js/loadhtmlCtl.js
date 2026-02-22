d96c50b06b804d2aca9f9adedc6659cadf98f227
Hemanth Rapeti <121911289+hemanthHemu33@users.noreply.github.com>
2026-02-22 22:44:22 +0530

    Copy top-level common JS vendor files

---
+++ web/common/js/loadhtmlCtl.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#includeLeftMenu").load("../common/layouts/leftMenu.html");    
+    $("#includeTopMenu").load("../common/layouts/topMenu.html");
+
+    $("#adminSubMenu").load("../common/layouts/adminSubMenu.html");
+
+}); 
